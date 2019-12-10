d315c5c68778a377612eef43af918f6762319dba
Purushotham Bantrothu <purushotham.sam@gmail.com>
2019-12-10 15:01:16 +0530

    changed css file to scss for card tile component

--- apps/enlight/src/app/components/card-tile/card-tile.component.ts
+++ apps/enlight/src/app/components/card-tile/card-tile.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit, Input } from '@angular/core';
 @Component({
   selector: 'workspace-card-tile',
   templateUrl: './card-tile.component.html',
-  styleUrls: ['./card-tile.component.css']
+  styleUrls: ['./card-tile.component.scss']
 })
 export class CardTileComponent implements OnInit {
   @Input() bookTitle;
