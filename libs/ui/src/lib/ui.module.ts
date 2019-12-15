9b4b3324f47eaebf0ec2497b8fc4b6850197b9e3
Purushotham Bantrothu <purushotham.sam@gmail.com>
2019-12-15 18:18:36 +0530

    Added required imports and declarartions to ui module

--- libs/ui/src/lib/ui.module.ts
+++ libs/ui/src/lib/ui.module.ts
@@ -3,11 +3,19 @@ import { CommonModule } from '@angular/common';
 import { CardTileComponent } from './card-tile/card-tile.component';
 import { RatingComponent } from './rating/rating.component';
 import { SearchBarComponent } from './search-bar/search-bar.component';
+import { AddCommasPipe, EllipsisPipe } from '@workspace/pipes';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
 
-  declarations: [CardTileComponent, RatingComponent, SearchBarComponent]
+  declarations: [
+    CardTileComponent,
+    RatingComponent,
+    SearchBarComponent,
+    EllipsisPipe,
+    AddCommasPipe
+  ]
 })
 export class UiModule {}
