7589a29208506a3664135cbf893ca6a5c66f1841
Purushotham Bantrothu <purushotham.sam@gmail.com>
2019-12-18 09:15:38 +0530

    Added test cases for sidenav component

--- apps/enlight/src/app/components/side-nav/side-nav.component.spec.ts
+++ apps/enlight/src/app/components/side-nav/side-nav.component.spec.ts
@@ -5,17 +5,43 @@ import { NO_ERRORS_SCHEMA } from '@angular/compiler/src/core';
 import { BooksFacade } from '../../+state/books.facade';
 import { StoreModule } from '@ngrx/store';
 import { RouterTestingModule } from '@angular/router/testing';
+import { of, Observable } from 'rxjs';
+import { SidenavToggleService } from '@workspace/libs/services';
+import { MatSidenav, MatSidenavModule } from '@angular/material';
+import { By } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BOOKS_CONSTANTS } from '../../constants/books_constants';
 
 describe('SideNavComponent', () => {
   let component: SideNavComponent;
   let fixture: ComponentFixture<SideNavComponent>;
+  // let booksFacade: BooksFacade;
+  let sideNav: MatSidenav;
+
+  const fakeBooksFacade = {
+    cartBooks$: new Observable(ob => {
+      ob.next([]);
+    }),
+    collectionBooks$: new Observable(ob => {
+      ob.next([]);
+    })
+  };
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       schemas: [NO_ERRORS_SCHEMA],
-      imports: [StoreModule.forRoot({}), RouterTestingModule],
+      imports: [
+        StoreModule.forRoot({}),
+        RouterTestingModule,
+        MatSidenavModule,
+        BrowserAnimationsModule
+      ],
       declarations: [SideNavComponent],
-      providers: [BooksFacade]
+      providers: [
+        { provide: BooksFacade, useValue: fakeBooksFacade },
+        SidenavToggleService,
+        MatSidenav
+      ]
     }).compileComponents();
   }));
 
@@ -23,9 +49,34 @@ describe('SideNavComponent', () => {
     fixture = TestBed.createComponent(SideNavComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    sideNav = fixture.debugElement.query(By.directive(MatSidenav))
+      .componentInstance;
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should update cart badge value to 2 when there are two books in cart', () => {
+    spyOn(sideNav, 'open');
+    component['booksFacade'].cartBooks$ = new Observable(ob => {
+      ob.next([{}, {}]);
+    });
+    component['showBadgeWhenBooksAddedToCart']();
+    component.navigationList.forEach(list => {
+      if (list['desc'] === BOOKS_CONSTANTS.SIDENAV_OPTION_CART)
+        expect(list['badgeValue']).toEqual(2);
+    });
+  });
+  it('should update collection badge value to 2 when there are two books in collection', () => {
+    spyOn(sideNav, 'open');
+    component['booksFacade'].collectionBooks$ = new Observable(ob => {
+      ob.next([{}, {}]);
+    });
+    component['showBadgeWhenBooksAddedToCollection']();
+    component.navigationList.forEach(list => {
+      if (list['desc'] === BOOKS_CONSTANTS.SIDENAV_OPTION_COLLECTION)
+        expect(list['badgeValue']).toEqual(2);
+    });
   });
 });
