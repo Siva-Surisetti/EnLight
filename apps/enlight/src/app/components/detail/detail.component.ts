6c5fa64989dcdde6ded7d48e71bd6be804dbdef1
Purushotham Bantrothu <purushotham.sam@gmail.com>
2019-12-18 12:19:51 +0530

    Code changes to verify if selected book already exists in cart or collection

--- apps/enlight/src/app/components/detail/detail.component.ts
+++ apps/enlight/src/app/components/detail/detail.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { BooksFacade } from '../../+state/books.facade';
 import { BOOKS_CONSTANTS } from '../../constants/books_constants';
+import { select } from '@ngrx/store';
 
 @Component({
   selector: 'poc-detail',
@@ -10,12 +11,47 @@ import { BOOKS_CONSTANTS } from '../../constants/books_constants';
 })
 export class DetailComponent implements OnInit {
   public selectedBook: any;
+  public cartBooks: any;
+  public collectionBooks: any;
+  public bookExistsInCart = false;
+  public bookExistsInCollection = false;
+
   constructor(private booksFacade: BooksFacade, private router: Router) {}
 
   ngOnInit() {
     this.booksFacade.selectedBook$.subscribe(book => {
       this.selectedBook = book;
     });
+    this.booksFacade.cartBooks$.subscribe(books => {
+      this.cartBooks = books;
+    });
+    this.booksFacade.collectionBooks$.subscribe(books => {
+      this.collectionBooks = books;
+    });
+
+    this.checkIfBookExistsInCart();
+
+    this.checkIfBookExistsInCollection();
+  }
+
+  private checkIfBookExistsInCollection() {
+    if (this.selectedBook && this.collectionBooks) {
+      this.collectionBooks.forEach(item => {
+        if (item.bookInfo.id === this.selectedBook.id) {
+          this.bookExistsInCollection = true;
+        }
+      });
+    }
+  }
+
+  private checkIfBookExistsInCart() {
+    if (this.selectedBook && this.cartBooks) {
+      this.cartBooks.forEach(book => {
+        if (book.id === this.selectedBook.id) {
+          this.bookExistsInCart = true;
+        }
+      });
+    }
   }
 
   addToCart() {
