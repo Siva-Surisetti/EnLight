ccf465d57a7db1b182993e9fc6dcfc7e3b0e4105
Purushotham Bantrothu <purushotham.sam@gmail.com>
2019-12-18 23:13:47 +0530

    Added test cases for add commas pipe

--- libs/pipes/src/lib/add-commas/add-commas.pipe.spec.ts
+++ libs/pipes/src/lib/add-commas/add-commas.pipe.spec.ts
@@ -1,8 +1,36 @@
 import { AddCommasPipe } from './add-commas.pipe';
 
 describe('AddCommasPipe', () => {
+  let pipe;
+  beforeEach(() => {
+    pipe = new AddCommasPipe();
+  });
   it('create an instance', () => {
-    const pipe = new AddCommasPipe();
     expect(pipe).toBeTruthy();
+  });
+
+  it('should return Author Unknown when authors are not available', () => {
+    const authors = pipe.transform(null);
+    expect(authors).toEqual('Author Unknown');
+  });
+
+  it('should return Author Unknown when empty array of authors is passed', () => {
+    const authors = pipe.transform([]);
+    expect(authors).toEqual('Author Unknown');
+  });
+
+  it('should return given author if one author is passed', () => {
+    const authors = pipe.transform(['Author']);
+    expect(authors).toEqual('Author');
+  });
+
+  it("should return authors with 'and' in between if two authors are passed", () => {
+    const authors = pipe.transform(['Author1', 'Author2']);
+    expect(authors).toEqual('Author1 and Author2');
+  });
+
+  it("should return authors with 'comma' and  'and' in between if three or more authors are passed", () => {
+    const authors = pipe.transform(['Author1', 'Author2', 'Author3']);
+    expect(authors).toEqual('Author1, Author2 and Author3');
   });
 });
