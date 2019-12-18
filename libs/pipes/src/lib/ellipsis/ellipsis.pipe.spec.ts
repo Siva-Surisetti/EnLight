6a0c8d8ba97a71c04f82a45cd43bc6b7d71067b9
Purushotham Bantrothu <purushotham.sam@gmail.com>
2019-12-18 23:22:55 +0530

    Added test cases for ellipsis pipe

--- libs/pipes/src/lib/ellipsis/ellipsis.pipe.spec.ts
+++ libs/pipes/src/lib/ellipsis/ellipsis.pipe.spec.ts
@@ -1,8 +1,31 @@
 import { EllipsisPipe } from './ellipsis.pipe';
 
 describe('EllipsisPipe', () => {
+  let pipe;
+  beforeEach(() => {
+    pipe = new EllipsisPipe();
+  });
   it('create an instance', () => {
-    const pipe = new EllipsisPipe();
     expect(pipe).toBeTruthy();
+  });
+
+  it('Should add ellipsis at the end when given string is greater than given length', () => {
+    const resultantString = pipe.transform('This is a test message', 7);
+    expect(resultantString).toEqual('This is...');
+  });
+
+  it('Should return empty string if empty string is passed regardless of limit value', () => {
+    const resultantString = pipe.transform('', 7);
+    expect(resultantString).toEqual('');
+  });
+
+  it('Should return same string if given limit value is greater than string length', () => {
+    const resultantString = pipe.transform('This is a test message', 50);
+    expect(resultantString).toEqual('This is a test message');
+  });
+
+  it('Should consider limit value as 250 if no value is passed to limit', () => {
+    const resultantString = pipe.transform('This is a test message');
+    expect(resultantString).toEqual('This is a test message');
   });
 });
